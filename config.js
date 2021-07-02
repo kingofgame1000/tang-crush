8598f4402adeb1fc0d507ad0bebe04b66017ee0e
kingofgame1000 <85267874+kingofgame1000@users.noreply.github.com>
2021-07-02 12:09:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Vì eanh quá ngầu và dtw <3',
+    reply: 'Vì anh quá ngầu và dtw <3',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Thế bây giờ chúng ta đã là người iu của nhau rùi nha.',
     btnAccept: 'Okiiiii lun <3',
