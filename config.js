019dd8ba2c48fce23237d6ce4fc273055cfdf31e
kingofgame1000 <85267874+kingofgame1000@users.noreply.github.com>
2021-07-02 12:07:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Em à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Anh có điều muốn nói với em`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Em đang thik anh có phải không😙',
+    desc: 'Phải trả lời thật lòng nha ',
     btnYes: 'Vâng,em rất là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Vì em thik anh <3',
+    reply: 'Vì eanh quá ngầu và dtw <3',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Thế bây giờ chúng ta đã là ng iu của nhau r nha.',
+    messDesc: 'Thế bây giờ chúng ta đã là người iu của nhau rùi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/100010557631588'  //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
